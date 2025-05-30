3625999f675775a06bd153f516862b2993ec0bf0
SVLOGer <max443584@gmail.com>
2025-05-30 04:23:41 +0300

    Поправил, чтобы был пул реквест

---
+++ Homework/ReviewFromLayout/src/utils/DefaultValues/DefaultRatings.ts
@@ -0,0 +1,20 @@
+import type {RatingType} from "../Types/Types.ts"
+
+export const defaultRatings: RatingType[] = [
+    {
+        title: "Чистенько", 
+        rating: 0
+    },{
+        title: "Сервис",
+        rating: 0
+    },{
+        title: "Скорость",
+        rating: 0
+    },{
+        title: "Место",
+        rating: 0
+    },{
+        title: "Культура речи",
+        rating: 0
+    }
+]
